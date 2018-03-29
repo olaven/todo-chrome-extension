604703a4aed50f4b952532bf59e4337558fc5146
olaven <olavsundfoer@gmail.com>
2018-03-29 12:18:58 +0200

    I think storage now works

--- todo.js
+++ todo.js
@@ -1,13 +1,22 @@
 const KEY_ENTER = 13; 
 
 let app = new Vue({
-    el : "#app", 
+    el : "#app",
     data : {
         header : "Todo app", 
         input : "",
         todos : [], 
         done : []
     }, 
+    init(){
+        chrome.storage.sync.get(["todos"], result => {
+            console.log(result.todos); 
+            app.todos = app.todos.concat(result.todos); 
+        });
+        chrome.storage.sync.get(["done"], result => {
+            app.done = app.done.concat(result.done); 
+        });
+    },
     methods : {
         handleKeyDown : (event) => {
             if(event.keyCode == KEY_ENTER){
@@ -22,13 +31,21 @@ let app = new Vue({
             app.input = "";
             //filter from done
             app.done = app.done.filter(d => d !== todo); 
-            app.todos.unshift(todo); 
+            app.todos.push(todo); 
+
+            app.updateStorage(); 
         }, 
         setDone : (event) => {
             let todo = event.target.innerHTML; 
             //filter from todos and add to done 
             app.todos = app.todos.filter(t => t !== todo); 
-            app.done.unshift(todo); 
+            app.done.unshift(todo);
+
+            app.updateStorage(); 
+        }, 
+        updateStorage: () => {
+            chrome.storage.sync.set({ todos: app.todos });
+            chrome.storage.sync.set({ done: app.done });
         }
     }
 }); 
